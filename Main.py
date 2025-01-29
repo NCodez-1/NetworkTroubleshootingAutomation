3d45950ff9976d7ac189e72f1cbaad2ed1cbad0d
Cyber-Waves <149782160+CyberJsec47@users.noreply.github.com>
2025-01-29 21:22:05 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,7 @@
 from netmiko import ConnectHandler
 from datetime import datetime
+import time
+from colorama import Fore, Style
 
 cisco_device = {
     'device_type': 'cisco_ios',
@@ -13,19 +15,40 @@ cisco_device = {
 def check_interfaces():
     try:
         connection = ConnectHandler(**cisco_device)
-        print(f"[{datetime.now}] Connected to {cisco_device['host']}")
-
         if 'secret' in cisco_device:
             connection.enable()
 
         output = connection.send_command("show ip interface brief")
         print(f"interface Status\n{output}")
         connection.disconnect()
-        print(f"[{datetime.now()}] Connection closed")
 
     except Exception as e:
         print(f"Error {e}")
 
 
+def show_vlan_brief():
+    try:
+        connection = ConnectHandler(**cisco_device)
+        if 'secret' in cisco_device:
+            connection.enable()
+
+        output = connection.send_command("show vlan brief")
+        print(f"Vlan Brief\n{output}")
+        connection.disconnect()
+    except Exception as e:
+        print(f"Error {e}")
+
+
 if __name__ == "__main__":
-    check_interfaces()
+    while True:
+        print(f"[{datetime.now}] Connected to {cisco_device['host']}")
+        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
+        check_interfaces()
+        print(Fore.BLUE + "-" * 50 + Style.RESET_ALL)
+        print(f"Waiting 10 seconds before next task")
+        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
+        time.sleep(10)
+        show_vlan_brief()
+        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
+        print(f"[{datetime.now()}] Connection closed")
+        break
