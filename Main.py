93e51faa2e8b380ae164208b944565264f27d282
Cyber-Waves <149782160+CyberJsec47@users.noreply.github.com>
2025-01-31 17:18:40 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,120 +1,82 @@
-from netmiko import ConnectHandler
+from pyfiglet import Figlet
+from CiscoFuncs import *
 from datetime import datetime
 import time
 from colorama import Fore, Style
-import structlog
-import logging
-import os
-
-project_dir = os.path.dirname(os.path.abspath(__file__))
-log_file_path = os.path.join(project_dir, "network_troubleshooting.json")
-
-logging.basicConfig(
-    filename=log_file_path,
-    level=logging.INFO,
-    format="%(message)s"
-)
-
-structlog.configure(
-    processors=[
-        structlog.processors.TimeStamper(fmt="iso"),
-        structlog.processors.JSONRenderer()
-    ],
-    logger_factory=structlog.stdlib.LoggerFactory(),
-    wrapper_class=structlog.make_filtering_bound_logger(logging.INFO),
-)
-
-log = structlog.get_logger()
-
-cisco_device = {
-    'device_type': 'cisco_ios',
-    'host': '############',
-    'username': '#####',
-    'password': '######',
-    'port': 22,
-    'secret': '######'}
 
 
-def check_interfaces():
-    try:
-        connection = ConnectHandler(**cisco_device)
-        if 'secret' in cisco_device:
-            connection.enable()
+def mainMenu():
+    while True:
+        f = Figlet(font='slant')
+        print(Fore.GREEN + f.renderText("COM 617") + Style.RESET_ALL)
+        print("Cisco Industrial consultancy project for COM617")
+        print("This project aims to automatically and remotely troubleshoot Cisco network devices")
+        print("The goals for this program is to remotely access a chosen device, run troubleshooting commands and "
+              "raise any errors detected and process these as logs which can be passed through the ELK stack "
+              "for visualization")
+        print(Fore.BLUE + "-" * 50)
+        print(Fore.GREEN + " Main Menu")
+        print(Fore.BLUE + "-" * 50)
+        print(Fore.GREEN + " Run automated scans               [1]")
+        print(Fore.BLUE + "-" * 50)
+        print(Fore.GREEN + " Select an interface to check      [2]")
+        print(Fore.BLUE + "-" * 50)
+        print(Fore.GREEN + " View log file contents            [3]")
+        print(Fore.BLUE + "-" * 50)
+        print(Fore.GREEN + " Exit                              [4]")
+        print(Fore.BLUE + "-" * 50)
 
-        output = connection.send_command("show ip interface brief")
-        print(f"interface Status\n{output}")
-        connection.disconnect()
+        option = int(input(Fore.GREEN + " Choose an option: "))
+        if option == 1:
+            print(Fore.GREEN + "Running automated tasks")
+            print(f"[{datetime.now}] Connected to {cisco_device['host']}")
 
-    except Exception as e:
-        print(f"Error {e}")
+            try:
+                while True:
+                    print(Fore.GREEN + "-" * 100 + Style.RESET_ALL)
+                    check_interfaces()
+                    print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
+                    print(f"Waiting 5 seconds before next task")
+                    print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
+                    time.sleep(5)
+                    show_vlan_brief()
+                    print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
+                    time.sleep(5)
+                    """ 
+                    This function would normally have a Ctrl+C to stop the running tasks but since that doesnt work in Pycharms terminal
+                    Ive added in a type exit to stop instead, In the final program this will be removed
+                    """
+                    user_input = input("Type 'exit' to stop: ").strip().lower()
+                    if user_input == "exit":
+                        print(Fore.RED + "Exiting automated tasks..." + Style.RESET_ALL)
+                        break
 
+            except KeyboardInterrupt:
+                print(Fore.GREEN + "\nExiting tasks" + Style.RESET_ALL)
+            mainMenu()
 
-def show_vlan_brief():
-    try:
-        connection = ConnectHandler(**cisco_device)
-        if 'secret' in cisco_device:
-            connection.enable()
+        elif option == 2:
+            log_interface()
+            mainMenu()
 
-        output = connection.send_command("show vlan brief")
-        print(f"Vlan Brief\n{output}")
-        connection.disconnect()
-    except Exception as e:
-        print(f"Error {e}")
+        elif option == 3:
+            lineCount = int(input("Choose number of lines to sample from file: " + Style.RESET_ALL))
+            Logs(lineCount)
+            option = input("Return to main or exit (M or E): ")
+            if option == ('m' or 'M'):
+                mainMenu()
+            else:
+                option == ('e' or 'E')
+                print(Fore.GREEN + f.renderText("Exiting"))
+                quit()
 
-
-def log_interface():
-    if_name = input("Select an interface: ")
-    try:
-        connection = ConnectHandler(**cisco_device)
-
-        if 'secret' in cisco_device:
-            connection.enable()
-
-        output = connection.send_command(f"show interface {if_name} status")
-        print(f"\nInterface Status:\n{output}\n")
-
-        if "notconnect" in output:
-            log.error(
-                "Interface Down",
-                interface=if_name,
-                status="Not Connected",
-                device=cisco_device["host"]
-            )
-        elif "connected" in output:
-            log.info(
-                "Interface is up",
-                interface=if_name,
-                status="Connected",
-                device=cisco_device["host"]
-            )
+        elif option == 4:
+            print(Fore.GREEN + f.renderText("Exiting"))
+            quit()
         else:
-            log.warning(
-                "Unknown Interface Status",
-                interface=if_name,
-                output=output,
-                device=cisco_device["host"]
-            )
-
-        connection.disconnect()
-
-    except Exception as e:
-        log.error("Connection failed", error=str(e), device=cisco_device["host"])
-        print(f"Error: {e}")
+            print("Choose an option")
 
 
 if __name__ == "__main__":
-    while True:
-        print(f"[{datetime.now}] Connected to {cisco_device['host']}")
-        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
-        check_interfaces()
-        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
-        print(f"Waiting 10 seconds before next task")
-        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
-        time.sleep(10)
-        show_vlan_brief()
-        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
-        log_interface()
-        print(Fore.BLUE + "-" * 100 + Style.RESET_ALL)
-        print(f"[{datetime.now()}] Connection closed")
-        break
+    mainMenu()
 
